12a4889d7368ee0c703919249de5e019651820cf
Kray Haslem <k.haslem@icloud.com>
2024-08-15 10:56:01 -0600

    Updated comments for node

--- node.py
+++ node.py
@@ -2,4 +2,4 @@ class Node:
     def __init__(self, initial_data):
         self.data = initial_data
         self.next = None
-        self.prev = None  # Not needed for singly linked lists
+        self.prev = None  # Not used for singly linked lists double linked only
